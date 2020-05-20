4a931083d8969812e66b755245b0bcdbdb519aa8
volkancicek <volkanericlice@gmail.com>
2020-05-20 17:56:44 +0200

    move methods to helper

--- unscented_kalman.py
+++ unscented_kalman.py
@@ -3,8 +3,8 @@ import datetime as dt
 from filterpy.common import Q_discrete_white_noise
 from filterpy.kalman import UnscentedKalmanFilter
 import numpy as np
-import matplotlib.pyplot as plt
 from filterpy.kalman import JulierSigmaPoints, MerweScaledSigmaPoints
+from helper import get_trend
 
 
 class UnscentedKalman:
@@ -17,14 +17,10 @@ class UnscentedKalman:
                                             (time.strftime('%d%m%Y-%H%M%S')))
         # self.sigmas = JulierSigmaPoints(n=1, kappa=1)
         self.sigmas = MerweScaledSigmaPoints(1, alpha=.1, beta=2., kappa=1.)
-
         self.ukf = UnscentedKalmanFilter(dim_x=1, dim_z=1, dt=1., hx=self.hx, fx=self.fx, points=self.sigmas)
-
         self.ukf.P = 1
-        self.ukf.R = r  # .01
+        self.ukf.R = r
         self.ukf.Q = q  # .0001  # process variance     # Q_discrete_white_noise(2, dt=1., var=0.03)
-        # self.ukf.R *= R  # estimate of measurement variance
-
         self.steps = steps
         self.measures, self.target, self.dates = measures, target, dates
         sz = (self.steps,)  # size of array
@@ -32,7 +28,6 @@ class UnscentedKalman:
         self.abs_errors = np.zeros(sz)
         self.squared_errors = np.zeros(sz)
         self.time_point = 0
-        self.trend_const = 1
 
     def fx(self, x, dt):
         x_out = np.empty_like(x)
@@ -41,22 +36,7 @@ class UnscentedKalman:
         return x_out
 
     def hx(self, x):
-        return x * self.get_trend(self.time_point) * self.trend_const
-
-    """ peak times: [07:30- 08:30], [16:30-17:30] """
-    """ peak ticks = [90, 102], [198, 210] """
-    """ ticks range 0-287 """
-    """ midnight tick => |0-90| = 90"""
-
-    def get_trend(self, point):
-        array = np.asarray([97, 205])
-        if 99 > point > 95 or 203 < point < 207:
-            return self.get_trend(point - 1)
-        else:
-            dist = (np.abs(array - point)).min()
-            """ transform points of day (288 -> 360) to degrees"""
-            dist_angle = dist * 1.25
-            return 2 / np.log(dist_angle)
+        return x * get_trend(self.time_point)
 
     def run_unscented_kalman(self):
         for k in range(self.steps):
@@ -76,24 +56,3 @@ class UnscentedKalman:
         print(str(self.target.min()))
 
         return mse, mae
-
-    def plot_results(self, normalized):
-        plt.figure()
-        if normalized:
-            self.measures = self.data.denormalize_measures(self.measures)
-            self.target = self.data.denormalize_target(self.target)
-            self.x = self.data.denormalize_target(self.x)
-
-        plt.plot(self.measures, 'k+', label='noisy measurements')
-        plt.plot(self.x, 'b-', label='prediction')
-        plt.plot(self.target, color='g', label='true value')
-        plt.legend()
-        plt.title('Prediction vs. iteration', fontweight='bold')
-        plt.xlabel('dates')
-        x_label_count = int(len(self.dates) / 70)
-        xi = list(range(x_label_count))
-        xi = [i * 70 for i in xi]
-        x_axes = [self.dates[i] for i in xi]
-        plt.xticks(xi, x_axes)
-        plt.ylabel('volume per hour')
-        plt.show()
